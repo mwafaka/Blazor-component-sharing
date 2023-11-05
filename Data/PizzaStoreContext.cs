05a906f8ee3a94756f694929c07b254f5295068c
mwafaka <mwafak.arbash@digitalcareerinstitute.org>
2023-11-05 14:41:25 +0100

    first commit

---
+++ Data/PizzaStoreContext.cs
@@ -0,0 +1,14 @@
+using BlazingPizza.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Data;
+
+public class PizzaStoreContext : DbContext
+{
+    public PizzaStoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<PizzaSpecial> Specials { get; set; }
+}
